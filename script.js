2ef6c19e74e18228105fae7f7e76ef9d7d19915c
Djaro Donk <djarodonk2004@gmail.com>
2023-08-30 15:07:13 +0200

    added arrow movement support
    Also made empty lines become empty again

--- script.js
+++ script.js
@@ -34,10 +34,31 @@ function next_line(){
     const detector = /line[1-9][0-9]*/;
     if (detector.test(act_el.id)) {
         var id_num = parseInt(act_el.id.slice(4,));
+        if (id_num > line_size) {
+            document.body.focus();
+        }
         console.log("line" + (id_num+1));
         document.getElementById("line" + (id_num+1)).focus();
     }
+}
 
+function previous_line(){
+    var act_el = document.activeElement;
+    if (act_el == document.body) {
+        document.getElementById("line1").focus();
+    }
+    const detector = /line[1-9][0-9]*/;
+    if (detector.test(act_el.id)) {
+        var id_num = parseInt(act_el.id.slice(4,));
+        if (id_num == 1) {
+            document.body.focus();
+        }
+        console.log("line" + (id_num+1));
+        document.getElementById("line" + (id_num-1)).focus();
+        if (act_el.innerText == "-\xa0") {
+            act_el.innerText = "";
+        }
+    }
 }
 
 function init(){
@@ -54,8 +75,33 @@ function init(){
     }
 }
 
+var shift = false;
+document.addEventListener('keyup', function (event) {
+    if (event.key === 'Shift') {
+        shift = false;
+    }
+});
+
 document.addEventListener('keydown', function (event) {
     if (event.key === 'Enter') {
+        event.preventDefault();
         next_line();
+    }
+    else if (event.key === 'ArrowDown') {
+        event.preventDefault();
+        next_line();
+    }
+    else if (event.key === 'ArrowUp') {
+        event.preventDefault();
+        previous_line();
+    } else if (event.key === 'Shift') {
+        shift = true;
+    } else if (event.key === "Tab") {
+        event.preventDefault();
+        if (shift) {
+            previous_line();
+        } else {
+            next_line();
+        }
     }
 });
